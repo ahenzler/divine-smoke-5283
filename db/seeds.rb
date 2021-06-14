c5e68703647d14c1324ff1a5df3732a109329d79
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-06-14 09:51:19 -0600

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Garden.destroy_all
+Plot.destroy_all
+Plant.destroy_all
+PlotsPlant.destroy_all
+
+
+@garden1 = Garden.create!(name: "Turing Community Garden", organic: true)
+@garden2 = Garden.create!(name: "Westminster Community Garden", organic: true)
+
+@plot1 = @garden1.plots.create!(number: 25, size: "Large", direction: "East")
+@plot2 = @garden2.plots.create!(number: 23, size: "Medium", direction: "West")
+
+@plant1 = Plant.create!( name: "Bell Pepper", description: "Prefers rich, well draining soil.", days_to_harvest: 90)
+@plant2 = Plant.create!( name: "Eggplant", description: "Prefers Sunlight, well watered soil.", days_to_harvest: 70)
+@plant3 = Plant.create!( name: "Cucumber", description: "Prefers Sunlight, well watered soil.", days_to_harvest: 50)
+
+PlotsPlant.create(plot_id: @plot1.id, plant_id: @plant1.id)
+PlotsPlant.create(plot_id: @plot1.id, plant_id: @plant2.id)
+PlotsPlant.create(plot_id: @plot2.id, plant_id: @plant2.id)
+PlotsPlant.create(plot_id: @plot2.id, plant_id: @plant3.id)
---
+++ spec/features/plots/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'index' do
+  before :each do
+
+    create
+    visit
+
+    end
+
+  describe 'class methods' do
+    describe '.' do
+    end
+
+  end
